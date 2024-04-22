1c32acf5866642a79269cd2f663bf0c3320ef289
Ludger Solbach <soulman@soulspace.org>
2024-04-22 08:46:53 +0200

    added parsing of avro json, renamed conversion functions

--- src/org/soulspace/schemashaper/adapter/avro.clj
+++ src/org/soulspace/schemashaper/adapter/avro.clj
@@ -1,5 +1,6 @@
 (ns org.soulspace.schemashaper.adapter.avro
   (:require [clojure.string :as str]
+            [clojure.set :as set]
             [clojure.data.json :as json]
             [org.soulspace.schemashaper.domain.model :as model]
             [org.soulspace.schemashaper.application.conversion :as conv]))
@@ -41,44 +42,97 @@
    ;:date-time-offset ""
    })
 
+(defn optional?
+  "Returns true, if the element is optional."
+  [e]
+  (let [t (:type e)
+        t-set (set t)]
+    (and (vector? t) (contains? t-set "null"))))
+
+(defn base-type
+  "Returns the base type of the element."
+  [e]
+  (let [t (:type e)
+        t-set (set t)
+        base-set (set/difference t-set #{"null"})]
+    (if (= 1 (count base-set))
+      (first base-set)
+      base-set)))
+
+(defn class-id
+  "Returns an id for the class."
+  [schema-ns name]
+  (keyword (str/join "/" [schema-ns name])))
+
+(defn namespace-id
+  "Returns an id for the class."
+  [name]
+  (let [parts (str/split name #"\.")
+        ns-parts (drop-last parts)
+        name-part (last parts)]
+   (keyword (str/join "/" [(str/join "." ns-parts) name-part]))))
+
 ;;
 ;; AVRO to model conversions
 ;;
-(defn avro-field->field
-  ""
-  [])
+(defn avro-field->model-field
+  "Returns a model field for the avro field."
+  [schema-ns e]
+  {:el :field
+   :name (:name e)
+   :optional (optional? e)
+   :type (base-type e)})
 
-(defn avro-record->class
-  ""
-  [])
+(defn avro-record->model-class
+  "Returns a model class for the avro record."
+  [schema-ns criteria e]
+  {:el :class
+   :id (class-id schema-ns (:name e))
+   :name (:name e)
+   :ct (into []
+             (map (partial avro-field->model-field schema-ns criteria)
+                  (:fields e)))})
+
+(defn avro-namespace->model-namespace
+  "Returns a model namespace for the avro namespace."
+  [criteria e]
+  {:el :namespace
+   :id (namespace-id (:name e))})
 
 ;;
 ;; Model to AVRO conversions
 ;;
-(defn field->avro-field
+(defn model-field->avro-field
   "Returns an avro field for the model field."
-  [e]
+  [schema-ns criteria e]
   (cond
     (:collection e)
     {:name (:name e)
      :type {:type "array"
             :values (get types->avro (:type e) (:type e))
             :default []}}
-    
+
     (:optional e)
     {:name (:name e)
      :type ["null" (get types->avro (:type e) (:type e))]}
-    
+
     :else
     {:name (:name e)
      :type (get types->avro (:type e) (:type e))}))
 
-(defn class->avro-record
+(defn model-class->avro-record
   "Returns an avro record for the model class."
-  [e]
+  [schema-ns criteria e]
   {:type "record"
    :name (:name e)
-   :fields (into [] (map field->avro-field (:ct e)))})
+   :fields (into []
+                 (map (partial model-field->avro-field schema-ns criteria)
+                      (:ct e)))})
+
+(defn model-namespace->avro-namespace
+  "Returns an avro namespace for the model namespace."
+  [criteria e]
+  {:namespace (:name e)})
 
 ;;
 ;; Conversion functions for AVRO
@@ -95,15 +149,17 @@
 (defmethod conv/model->schema :avro
   ([format coll]
    (conv/model->schema format {} coll))
-  ([format filter coll]
+  ([format criteria coll]
    (->> coll
-        (map class->avro-record)
+        (map (partial model-class->avro-record "default" criteria))
         (into [])
-        (json/json-str))))
+        (json/write-str))))
 
 (comment
-  (json/read-json "test-avro.json")
-  (json/json-str (conv/model->schema :avro [{:el :class :name "TestClass" :ct [{:el :field :name "id" :type :int :optional true}]}]))
+  (json/read-str (slurp "examples/sap-sample-avro.json") :key-fn keyword)
+  (println (json/write-str (conv/model->schema :avro [{:el :class :name "TestClass" :ct [{:el :field :name "id" :type :int :optional true}]}])))
+  (optional? {:name "Notes", :type ["null" "string"]})
+  (optional? {:name "SessionID", :type "long"})
   ;(json/read)
   ;
   )

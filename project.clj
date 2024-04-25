fa931379d25012ccf65ef8d4ed67ffb5d80e9640
Ludger Solbach <soulman@soulspace.org>
2024-04-26 00:31:13 +0200

    added dependencies for protobuf and openapi input

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject schemashaper "0.1.0-SNAPSHOT"
   :description "Tool for schema conversions"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/data.json "2.3.0"]
                  [org.clojure/tools.cli "1.0.206"]
+                 [org.clojure/data.xml "0.0.8"]            ; EDMX/OData
+                 [org.clojure/data.json "2.3.0"]           ; AVRO
+                 [clj-commons/clj-yaml "1.0.27"]           ; OpenAPI
+                 [com.github.s-expresso/rubberbuf "0.2.1"] ; ProtoBuf
                  ;[instaparse "1.4.14"]
                  ]
   :repl-options {:init-ns org.soulspace.schemashaper.adapter.ui.cli}

f0c340e8db578c20c059e73da002f2de7ac71b22
Solbach, Ludger (FDP4_EXTERN) <ludger.solbach@msg.group>
2024-05-07 17:26:20 +0200

    added openapi sample file, enhanced openapi input

--- src/org/soulspace/schemashaper/adapter/input/openapi.clj
+++ src/org/soulspace/schemashaper/adapter/input/openapi.clj
@@ -1,11 +1,16 @@
 (ns org.soulspace.schemashaper.adapter.input.openapi
-  (:require [clj-yaml.core :as yaml]))
+  (:require [clj-yaml.core :as yaml]
+            [org.soulspace.schemashaper.application.conversion :as conv]))
 
 (def openapi->types
-  {"integer" "int"
-   ["integer" "int32"] "int"
-   ["integer" "int64"] "long"
-   "string" "string"
+  ""
+  {"integer"             "int"
+   "string"              "string"
+   ["integer" "int32"]   "int"
+   ["integer" "int64"]   "long"
+   ["number" "float"]    "float"
+   ["number" "double"]   "double"
+;   ["string" "password"] 
    ;"array" ""
    ;"object" ""
    ;
@@ -13,8 +18,33 @@
 
 (defn openapi-type
   "Dispatch function."
-  [e]
-  )
+  [[k v]]
+  (keyword k))
+
+(defn openapi-property->overarch
+  ""
+  [[k v]]
+  (let [p-name k
+        p-type ""
+        ;
+        ]
+    {:el :field
+     :name p-name
+     :type p-type
+     }))
+
+(defn openapi-schema->overarch
+  ""
+  [[k v]]
+  (let [e-name k
+        e-id k ; TODO generate id
+        e-desc (get (:description v) "")
+        ;
+        ]
+    {:el :class
+     :id e-id ;
+     :name e-name
+     :desc e-desc}))
 
 (defmulti openapi->overarch
   "Converts an OpenAPI element into an overarch class model element."
@@ -22,29 +52,28 @@
 
 (defmethod openapi->overarch :openapi
   [[k v]]
-  (println "openapi"))
+  (println "openapi" k))
 
 (defmethod openapi->overarch :info
   [[k v]]
-  (println "info"))
+  (println "info" k))
 
 (defmethod openapi->overarch :servers
   [[k v]]
-  (println "servers"))
+  (println "servers" k))
 
 (defmethod openapi->overarch :paths
   [[k v]]
-  (println "paths"))
+  (println "paths" k))
 
 (defmethod openapi->overarch :components
   [[k v]]
-  (println "components")
-  )
+  (println "components" k)
+  (map openapi->overarch (:schemas v)))
 
 (defmethod openapi->overarch :schemas
   [[k v]]
-  (println "schemas"))
-
+  (println "schemas" k))
 
 (defmethod conv/schema->model :openapi
   ([format input]
@@ -52,10 +81,19 @@
   ([format config input]
    (let [openapi (yaml/parse-string input)
          _ (println "OpenAPI" openapi)
-         model []]
+         paths (:paths openapi)
+         _ (println "Components" (:components openapi))
+         components (:components openapi)
+         _ (println "Schema" (:schemas components))
+         schemas (:schemas components)
+         ;model (map openapi->overarch (:components openapi))
+         _ (doseq [[k v] schemas] (println "Schema" k v))
+         model []
+         ;
+         ]
      model)))
 
 (comment
-  ()
+  (conv/schema->model :openapi (slurp "examples/petstore-openapi-30.yaml"))
   ;
   )
